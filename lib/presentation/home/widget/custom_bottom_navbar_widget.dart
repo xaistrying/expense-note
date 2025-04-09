dbadc4b29928c19bae4301f919294702ce547c2f
xaistrying <xaistrying@gmail.com>
2025-04-09 23:34:59 +0700

    wip: animation for bottom navbar

--- lib/presentation/home/widget/custom_bottom_navbar_widget.dart
+++ lib/presentation/home/widget/custom_bottom_navbar_widget.dart
@@ -15,26 +15,38 @@ class CustomBottomNavbarWidget extends StatefulWidget {
 }
 
 class _CustomBottomNavbarWidgetState extends State<CustomBottomNavbarWidget>
-    with SingleTickerProviderStateMixin {
+    with TickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<Offset> _slideAnimation;
+
+  late AnimationController _navController;
+  late Animation<Offset> _navSlideAnimation;
 
   @override
   void initState() {
     super.initState();
 
     _controller = AnimationController(duration: Durations.medium1, vsync: this);
+    _navController = AnimationController(
+      duration: Durations.medium1,
+      vsync: this,
+    );
 
     _slideAnimation = Tween<Offset>(
       begin: Offset(0, 1),
       end: Offset(0, 0),
     ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+    _navSlideAnimation = Tween<Offset>(
+      begin: Offset(0, 0),
+      end: Offset(0, 1),
+    ).animate(CurvedAnimation(parent: _navController, curve: Curves.easeInOut));
   }
 
   @override
   void dispose() {
     super.dispose();
     _controller.dispose();
+    _navController.dispose();
   }
 
   @override
@@ -42,48 +54,140 @@ class _CustomBottomNavbarWidgetState extends State<CustomBottomNavbarWidget>
     return BlocListener<HandleTileCubit, HandleTileState>(
       listener: (context, state) {
         if (state.data.selectedTileIndex != null) {
-          _controller.forward();
+          _navController.forward();
+          Future.delayed(Durations.medium1, () {
+            _controller.forward();
+          });
         } else {
           _controller.reverse();
+          Future.delayed(Durations.medium1, () {
+            _navController.reverse();
+          });
         }
       },
       child: Container(
         height: 60,
         decoration: BoxDecoration(color: AppColor.customBrown1),
-        child: SlideTransition(
-          position: _slideAnimation,
-          child: Row(
-            children: [
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.all(AppDimens.padding4),
-                  height: double.infinity,
-                  child: IconButton(
-                    onPressed:
-                        () => context.read<HandleTileCubit>().updateValue(
-                          UpdateValueType.add,
-                        ),
-                    style: _customButtonStyle(),
-                    icon: Icon(MingCute.add_fill),
+        child: Stack(
+          children: [
+            // Nav
+            SlideTransition(
+              position: _navSlideAnimation,
+              child: Row(
+                children: [
+                  // Left
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.only(
+                        left: AppDimens.padding4,
+                        top: AppDimens.padding4,
+                        bottom: AppDimens.padding4,
+                        right: AppDimens.padding2,
+                      ),
+                      height: double.infinity,
+                      child: IconButton(
+                        onPressed:
+                            () => context.read<HandleTileCubit>().updateValue(
+                              UpdateValueType.add,
+                            ),
+                        style: _customButtonStyle(),
+                        icon: Icon(MingCute.diary_fill),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.all(AppDimens.padding4),
-                  height: double.infinity,
-                  child: IconButton(
-                    onPressed:
-                        () => context.read<HandleTileCubit>().updateValue(
-                          UpdateValueType.minus,
-                        ),
-                    style: _customButtonStyle(),
-                    icon: Icon(MingCute.minimize_fill),
+                  // Middle
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.only(
+                        left: AppDimens.padding2,
+                        top: AppDimens.padding4,
+                        bottom: AppDimens.padding4,
+                        right: AppDimens.padding2,
+                      ),
+                      height: double.infinity,
+                      child: IconButton(
+                        onPressed:
+                            () => context.read<HandleTileCubit>().updateValue(
+                              UpdateValueType.add,
+                            ),
+                        style: _customButtonStyle(),
+                        icon: Icon(MingCute.chart_vertical_fill),
+                      ),
+                    ),
                   ),
-                ),
+                  // Right
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.only(
+                        left: AppDimens.padding2,
+                        top: AppDimens.padding4,
+                        bottom: AppDimens.padding4,
+                        right: AppDimens.padding4,
+                      ),
+                      height: double.infinity,
+                      child: IconButton(
+                        onPressed:
+                            () => context.read<HandleTileCubit>().updateValue(
+                              UpdateValueType.add,
+                            ),
+                        style: _customButtonStyle(),
+                        icon: Icon(MingCute.settings_4_fill),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+
+            // Function
+            SlideTransition(
+              position: _slideAnimation,
+              child: Row(
+                children: [
+                  // Left
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.only(
+                        left: AppDimens.padding4,
+                        top: AppDimens.padding4,
+                        bottom: AppDimens.padding4,
+                        right: AppDimens.padding2,
+                      ),
+                      height: double.infinity,
+                      child: IconButton(
+                        onPressed:
+                            () => context.read<HandleTileCubit>().updateValue(
+                              UpdateValueType.add,
+                            ),
+                        style: _customButtonStyle(),
+                        icon: Icon(MingCute.add_fill),
+                      ),
+                    ),
+                  ),
+                  // Right
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.only(
+                        left: AppDimens.padding2,
+                        top: AppDimens.padding4,
+                        bottom: AppDimens.padding4,
+                        right: AppDimens.padding4,
+                      ),
+                      height: double.infinity,
+                      child: IconButton(
+                        onPressed:
+                            () => context.read<HandleTileCubit>().updateValue(
+                              UpdateValueType.minus,
+                            ),
+                        style: _customButtonStyle(),
+                        icon: Icon(MingCute.minimize_fill),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
